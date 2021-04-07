3ace4a86388f0db5a945c3018addb414e2d42d6f
Filipe Pinto Reis <filipepintodosreis@gmail.com>
2021-04-07 22:12:06 +0100

    compiling agent script

---
+++ scripts/compileAgent.py
@@ -0,0 +1,13 @@
+from shutil import copyfile
+from os import system
+
+AGENT_NAME = "DiCaprio"
+VERSION = "1.7.0"
+
+system(f"cd {AGENT_NAME} && mvn clean package && cd ..")
+
+from_path = f"{AGENT_NAME}/target/{AGENT_NAME}-{VERSION}.jar"
+to_path = f"brokers/{AGENT_NAME}/{AGENT_NAME}-{VERSION}.jar"
+
+copyfile(from_path, to_path)
+
