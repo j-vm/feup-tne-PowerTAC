0dd161bcd92947dd9fb234b7fb2f84b7cbbd3ce7
j-vm <joaoviegamacedo@gmail.com>
2021-05-03 17:28:10 +0100

    updated mlmodel files

--- temporaryName/src/main/java/org/powertac/samplebroker/mlmodel/DQNManager.java
+++ temporaryName/src/main/java/org/powertac/samplebroker/mlmodel/DQNManager.java
@@ -1,43 +1,40 @@
 package org.powertac.samplebroker.mlmodel;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.deeplearning4j.rl4j.learning.sync.Transition;
 import org.deeplearning4j.rl4j.learning.sync.qlearning.discrete.TDTargetAlgorithm.DoubleDQN;
+import org.deeplearning4j.rl4j.observation.Observation;
+import org.nd4j.linalg.api.ndarray.INDArray;
 
 public class DQNManager {
 
 	private DoubleDQN doubleDQN;
 	private DQNSource source;
 
-	private Transition<Integer> lastTransition;
-	private List<Transition<Integer>> transitions;
+	private Map<Integer, Observation> Observations;
 	
+	private List<Transition<Integer>> transitions = new ArrayList<Transition<Integer>>();
+
 	public DQNManager(DQNSource source) {
 		super();
 		this.source = source;
-		this.doubleDQN = new DoubleDQN(source, 1, 1); //VIEW OTHER PARAMS
+		this.doubleDQN = new DoubleDQN(source, 1, 1); // VIEW OTHER PARAMS
 	}
-	
-	public void predict() {
-		
+
+	public void predict(INDArray inputs, int timeSlot) {
+		Observation obs = new Observation(inputs);
+		this.source.getQNetwork().output(obs);
 	}
-	
+
 	public void fit() {
 		
 	}
-	
+
 	public void sync() {
-		this.doubleDQN.computeTDTargets(null);
-	}
-	
-	public void loadModel() {
-		
+		this.doubleDQN.computeTDTargets(this.transitions);
 	}
 
-	public void saveModel() {
-		
-	}
-	
-	
 }
--- temporaryName/src/main/java/org/powertac/samplebroker/mlmodel/DQNSource.java
+++ temporaryName/src/main/java/org/powertac/samplebroker/mlmodel/DQNSource.java
@@ -1,4 +1,11 @@
 package org.powertac.samplebroker.mlmodel;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileOutputStream;
+
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.layers.DenseLayer;
@@ -17,56 +24,94 @@ import org.nd4j.linalg.learning.config.Nesterovs;
 import org.nd4j.linalg.learning.config.RmsProp;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 
+public class DQNSource implements TargetQNetworkSource {
 
-public class DQNSource implements TargetQNetworkSource{
-	
-	
-	private int[] shape = {2, 3, 4};
-	private	MultiLayerConfiguration multiLayerConf = new NeuralNetConfiguration.Builder()
-			  .seed(123)
-			  .updater(new Nesterovs(0.1, 0.9)) //High Level Configuration
-			  .list() //For configuring MultiLayerNetwork we call the list method
-			  .layer(0, new DenseLayer.Builder().nIn(6).nOut(6).weightInit(WeightInit.XAVIER).activation(Activation.RELU).build()) //Configuring Layers
-			  .layer(1, new OutputLayer.Builder(LossFunctions.LossFunction.XENT).nIn(6).nOut(3).weightInit(WeightInit.XAVIER).activation(Activation.SIGMOID).build())
-			  .build(); //Building Configuration
-	
+	private int[] shape = { 2, 3, 4 };
+	private MultiLayerConfiguration multiLayerConf = new NeuralNetConfiguration.Builder().seed(123)
+			.updater(new Nesterovs(0.1, 0.9)) // High Level Configuration
+			.list() // For configuring MultiLayerNetwork we call the list method
+			.layer(0,
+					new DenseLayer.Builder().nIn(6).nOut(6).weightInit(WeightInit.XAVIER).activation(Activation.RELU)
+							.build()) // Configuring Layers
+			.layer(1, new OutputLayer.Builder(LossFunctions.LossFunction.XENT).nIn(6).nOut(3)
+					.weightInit(WeightInit.XAVIER).activation(Activation.SIGMOID).build())
+			.build(); // Building Configuration
+
 	private MultiLayerNetwork predictMLN = new MultiLayerNetwork(this.multiLayerConf);
-	private DQN predictor = new DQN<>(this.predictMLN);
+	private DQN predictor;
 	private MultiLayerNetwork targetMLN = new MultiLayerNetwork(this.multiLayerConf);
-	private DQN target = new DQN<>(this.targetMLN);
+	private DQN target;
 
-	
 	public void createModel() {
-		
-	}
-	
-	public void predict() {
-		
-	}
-	
-	public void fit() {
-		
-	}
-	
-	public void sync() {
-		
-	}
-	
-	public void loadModel() {
-		
+		this.predictor = new DQN<>(this.predictMLN);
+		this.target = new DQN<>(this.targetMLN);
+		this.predictor.reset();
+		this.target.reset();
 	}
 
-	public void saveModel() {
-		
+	public void loadModel(String filename) {
+		try {
+			// constructor of File class having file as argument
+			File file = new File(filename);
+			// creates a buffer reader input stream
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String model = "";
+			int r = 0, i = 0;
+			while ((r = br.read()) != -1) {
+				model = addChar(model, (char) r, i);
+				i++;
+			}
+			this.predictor = DQN.load(model);
+			this.target = DQN.load(model);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void saveModel(String filename) {
+		  FileOutputStream fos = null;
+	      File file;
+	      try {
+			  file = new File(filename);
+			  fos = new FileOutputStream(file);
+
+			  if (!file.exists()) {
+			     file.createNewFile();
+			  }
+			  
+			  this.predictor.save(fos); 
+			  fos.flush(); 
+			  System.out.println("DQN Model Saved Successfully to:  " + filename);
+	      } 
+	      catch (IOException ioe) {
+	    	  ioe.printStackTrace();
+	      } 
+	      finally {
+			  try {
+			     if (fos != null) {
+			    	 fos.close();
+			     }
+		      } 
+			  catch (IOException ioe) {
+			     System.out.println("Error in closing DQN File Output Stream");
+			  }
+	      }
 	}
 
 	@Override
 	public IDQN getQNetwork() {
-		return (IDQN)this.predictor;
+		return (IDQN) this.predictor;
 	}
 
 	@Override
 	public IDQN getTargetQNetwork() {
-		return (IDQN)this.target;
+		return (IDQN) this.target;
 	}
+
+	private String addChar(String str, char ch, int position) {
+		StringBuilder sb = new StringBuilder(str);
+		sb.insert(position, ch);
+		return sb.toString();
+	}
+
 }
