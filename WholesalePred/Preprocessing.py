3b63c0bc1f8e905dca3659bf265d1a861f5e4abf
Filipe Pinto Reis <filipepintodosreis@gmail.com>
2021-05-14 19:39:33 +0100

    Preprocessing class

---
+++ WholesalePred/Preprocessing.py
@@ -0,0 +1,8 @@
+class Preprocessing:
+    @staticmethod
+    def format_transform(data):
+        '''
+        This method must take a json-like dictionary and transform what was received into a plain list of features.
+        If there are any non-number like inputs, they should be mapped here as well.
+        '''
+        pass
