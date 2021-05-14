f9860ab67e1c47bd5c395c4781a0975684439748
Filipe Pinto Reis <filipepintodosreis@gmail.com>
2021-05-14 19:39:23 +0100

    Added model class

--- WholesalePred/Model.py
+++ WholesalePred/Model.py
@@ -5,6 +5,28 @@ class Model:
     def __init__(self, name, model):
         self.name = name
         self.model = model
+        self.save_model()
+
+    def single_sample_train(self, data):
+        self.model = Model.load_model(self.name)
+        self.train(data)
+        self.save_model()
+    
+    def single_sample_predict(self, data):
+        self.model = Model.load_model(self.name)
+        return self.predict(data)
+
+    def train(self, data):
+        if "train" not in self.model.__dir__():
+            raise("The model should implement train method")
+
+        self.model.train(data)
+    
+    def predict(self, data):
+        if "predict" not in self.model.__dir__():
+            raise("The model should implement predict method")
+
+        return self.model.predict(data)
     
     def save_model(self):
         file_path = f'WholesalePred/model_data/{self.name}'
