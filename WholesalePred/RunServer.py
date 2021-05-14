d7da69144265a23a4694f7f0a40cbc15c2a1a79b
Filipe Pinto Reis <filipepintodosreis@gmail.com>
2021-05-14 12:59:36 +0100

    Server parsing messages into json

--- WholesalePred/RunServer.py
+++ WholesalePred/RunServer.py
@@ -15,14 +15,15 @@ class OurBaseHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
-        
+        json_string = post_data.decode('utf-8')
+
         print(f"""POST request,
         Headers:
         {str(self.headers)}
-        
-        Body:
-        {post_data.decode('utf-8')}
         """)
+
+        aDict = json.loads(json_string)
+        print(aDict)
 
         self._set_OK_response()
         self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
