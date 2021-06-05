Fixed data normalization

csv_normalization.py
@@ -1,7 +1,10 @@
 from sklearn.preprocessing import StandardScaler
+import numpy as np
+
+arr_to_normalize = []
+arr_not = []
 
 with open("WholesalePred/data.csv", "r") as f:
-    arr = []
     i = 0
     for line in f.readlines():
         if i == 0:
@@ -11,11 +14,22 @@ with open("WholesalePred/data.csv", "r") as f:
         
         new_arr = [el for el in line.split(",")]
         
-        arr.append(new_arr)
+        arr_to_normalize.append(new_arr[:len(new_arr) - 1])
+        arr_not.append(new_arr[len(new_arr) - 1:])
 
     scaler = StandardScaler()
 
-    scaler.fit_transform(arr)
+    arr_to_normalize = scaler.fit_transform(arr_to_normalize)
+    
+    for i in range(len(arr_to_normalize)):
+        for j in range(len(arr_to_normalize[i])):
+            arr_to_normalize[i][j] = str(float(arr_to_normalize[i][j]) * 1000)
+
+
+    arr = []
+
+    for i in range(len(arr_to_normalize)):
+        arr.append(np.concatenate((arr_to_normalize[i], np.array(arr_not[i]))))
 
     with open("WholesalePred/data2.csv", "w") as f2:
         f2.write(first_line)
@@ -24,4 +38,3 @@ with open("WholesalePred/data.csv", "r") as f:
                 f2.write(str(l))
                 if i != len(el) - 1:
                     f2.write(",")
-            f2.write('\n')